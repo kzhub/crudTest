587f36fdd6f2b491170adf434bcc51ae5c5ec3fa
kzhub <kazukicoronek@gmail.com>
2023-02-24 21:53:39 +0900

    2

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get started 2 by editing&nbsp;
             <code className={styles.code}>pagit push -u origins/index.tsx</code>
           </p>
           <div>
